b139db7f1b316d4fc00ec03bf5ae1110e91e1146
aklema094 <aklemasultana094@gmail.com>
2024-12-28 15:41:33 +0600

    initial commit

---
+++ src/snakegame/Snake.java
@@ -0,0 +1,17 @@
+
+package snakegame;
+
+import javax.swing.JFrame;
+
+
+public class Snake extends JFrame {
+    
+    
+    public static void main(String[] args) {
+     
+       
+       
+       
+    }
+    
+}
