973f494046e671d1e844e65edb0b6bc9bbfa9584
aklema094 <aklemasultana094@gmail.com>
2024-12-28 17:50:05 +0600

    set snake and food on board

--- src/snakegame/Board.java
+++ src/snakegame/Board.java
@@ -2,13 +2,114 @@
 package snakegame;
 
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
-public class Board extends JPanel {
+public class Board extends JPanel implements ActionListener{
+    
+    private int dots;
+    private final int allDots = 2500;
+    private final int dotSize = 14;
+    private final int rendomPos = 30;
+    private int x[] = new int[allDots];
+    private int y[] = new int[allDots];
+    private Image apple;
+    private Image head;
+    private Image body;
+    private int point =0;
+    private int appleX;
+    private int appleY;
+    private Timer timer;
+    private boolean left = false;
+    private boolean right = true;
+    private boolean up = false;
+    private boolean down = false;
+    
 
     public Board() {
         setBackground(Color.BLACK);
+        setFocusable(true);
+
+        loadImages();
+        startGame();
     }
+    private void loadImages() {
+        ImageIcon i1 = new ImageIcon(getClass().getResource("apple.png"));
+        apple = i1.getImage();
+        ImageIcon i2 = new ImageIcon(getClass().getResource("head.png"));
+        head = i2.getImage();
+        ImageIcon i3 = new ImageIcon(getClass().getResource("dot.png"));
+        body = i3.getImage();
+    }
+
+    private void startGame() {
+        dots = 3;
+        for (int i = 0; i < dots; i++) {
+            y[i] = 50;
+            x[i] = 50-i*dotSize;    
+        }
+      locateApple();
+      timer = new Timer(140,this);
+      timer.start();
+      
+    }
+     @Override
+    public void actionPerformed(ActionEvent e) {
+        move();
+        repaint();
+    }
+    public void move(){
+        for (int i = dots; i>0; i--) {
+            x[i] = x[i-1];
+            y[i] = y[i-1];
+        }
+        if(left){
+            x[0] -= dotSize;
+        }
+        if(right){
+            x[0] += dotSize;
+        }
+        if(up){
+            y[0] -= dotSize;
+        }
+        if(down){
+            y[0] += dotSize;
+        }
+        
+    }
+    
+    public void locateApple(){
+        int r = (int) (Math.random()*rendomPos);
+        appleX = r*10;
+        r = (int) (Math.random()*rendomPos);
+        appleY = r*10;
+    }
+    
+    
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        g.drawImage(apple, appleX, appleY, this);
+        for (int i = 0; i < dots; i++) {
+            if (i==0) {
+                g.drawImage(head, x[i], y[i], this);
+            }else{
+                 g.drawImage(body, x[i], y[i], this);
+            } 
+        }
+        Toolkit.getDefaultToolkit().sync();
+    }
+
+
+
+   
+
+    
     
 }
 
--- src/snakegame/Snake.java
+++ src/snakegame/Snake.java
@@ -1,22 +1,28 @@
 
 package snakegame;
-
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class Snake extends JFrame {
+    private ImageIcon icon;
     
     Snake(){ 
+        setTitle("Snake Game");
+        icon = new ImageIcon(getClass().getResource("Snake.png"));
+        this.setIconImage(icon.getImage());
         add(new Board());
         pack();
-        setSize(400,400);
+        setSize(500,500);
         setLocationRelativeTo(null);
         setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);     
     }
     public static void main(String[] args) {
-     
+    
           new Snake().setVisible(true);
        
     }
+
+    
     
 }
