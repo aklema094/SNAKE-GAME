a631665ae4f2c4b47b21e448e6d0bae02bd4c383
aklema094 <aklemasultana094@gmail.com>
2024-12-28 15:47:22 +0600

    added board

---
+++ src/snakegame/Board.java
@@ -0,0 +1,14 @@
+
+package snakegame;
+
+import java.awt.Color;
+import javax.swing.JPanel;
+
+public class Board extends JPanel {
+
+    public Board() {
+        setBackground(Color.BLACK);
+    }
+    
+}
+
--- src/snakegame/Snake.java
+++ src/snakegame/Snake.java
@@ -3,14 +3,19 @@ package snakegame;
 
 import javax.swing.JFrame;
 
-
 public class Snake extends JFrame {
     
-    
+    Snake(){ 
+        add(new Board());
+        pack();
+        setSize(400,400);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);     
+    }
     public static void main(String[] args) {
      
-       
-       
+          new Snake().setVisible(true);
        
     }
     
